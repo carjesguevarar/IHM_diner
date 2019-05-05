e829d145a21c3d9ab638cb9e3f5691704dbbbacc
Carlos Guevara <carjesguevarar@gmail.com>
2019-05-05 08:03:52 -0400

    Ajustes heroku DB.

--- ihm_diner/settings.py
+++ ihm_diner/settings.py
@@ -77,12 +77,13 @@ WSGI_APPLICATION = 'ihm_diner.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
+
 
 
 
