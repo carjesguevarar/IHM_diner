78c5ed0b48cd3506fd770640afd9eacce7189ab3
Carlos Guevara <carjesguevarar@gmail.com>
2019-05-18 06:44:05 -0400

    Forms fix.

--- ihm_diner/settings.py
+++ ihm_diner/settings.py
@@ -79,10 +79,12 @@ WSGI_APPLICATION = 'ihm_diner.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
 
 
 
